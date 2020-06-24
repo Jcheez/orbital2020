4de9c905c0540a2dc13d119ec9b5c3be9603abb2
Jia Jun <jiajunlee2@gmail.com>
2020-06-24 22:43:40 +0800

    COde change to package.json

--- Backend/server.js
+++ Backend/server.js
@@ -41,7 +41,7 @@ const currenciesRouter = require("./routes/currencies");
 app.use("/currencies", currenciesRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/build"));
+  app.use(express.static("build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "build", "index.html"));
