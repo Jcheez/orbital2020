ba0ad66a5c3a58815d5b590531d8f1a72676b79c
Jia Jun <jiajunlee2@gmail.com>
2020-06-24 22:22:12 +0800

    V1.1

--- Backend/server.js
+++ Backend/server.js
@@ -41,10 +41,10 @@ const currenciesRouter = require("./routes/currencies");
 app.use("/currencies", currenciesRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }
 
