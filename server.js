a4ba06a8aecdc3b90562a870fb73f3946d5f68c9
Jia Jun <jiajunlee2@gmail.com>
2020-06-25 00:04:57 +0800

    V1.2

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require("path");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(
   bodyParser.urlencoded({
